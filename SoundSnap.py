a27c8b5a200068b04f21b8d10c2d9bd1d9ce2939
Abhi Patel <patelabhi10699@gmail.com>
2023-11-02 01:01:21 +0000

    feat: added image preview for good user experience

--- SoundSnap.py
+++ SoundSnap.py
@@ -20,23 +20,29 @@ image_file = st.file_uploader(
 
 if image_file:
   songs = recommend_songs(image_file) 
-  for song_id in songs:
-    components.html(
-      f"""
-      <iframe 
-        style="border-radius:12px" 
-        src="https://open.spotify.com/embed/track/{song_id}?utm_source=generator" 
-        width="100%"
-        height="100"
-        frameBorder="0" 
-        allowfullscreen="" 
-        allow="autoplay; 
-        clipboard-write; 
-        encrypted-media; 
-        fullscreen; 
-        picture-in-picture" 
-        loading="lazy">
-      </iframe>
-      """,
-      height=100
-    )
+  col1, col2 = st.columns(2)
+
+  with col1:
+    st.image(image_file)
+
+  with col2:
+    for song_id in songs:
+      components.html(
+        f"""
+        <iframe 
+          style="border-radius:12px" 
+          src="https://open.spotify.com/embed/track/{song_id}?utm_source=generator" 
+          width="100%"
+          height="190"
+          frameBorder="0" 
+          allowfullscreen="" 
+          allow="autoplay; 
+          clipboard-write; 
+          encrypted-media; 
+          fullscreen; 
+          picture-in-picture" 
+          loading="lazy">
+        </iframe>
+        """,
+        height=200
+      )
